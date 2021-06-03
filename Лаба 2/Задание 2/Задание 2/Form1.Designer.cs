958565a80d5c5bfc9fd3210e1f4b591a97a4fdfb
GamerRiT <alex-kashin-03@mail.ru>
2021-06-03 22:41:33 +0300

    Лаба2
    Не работает с 3 предложения

---
+++ Лаба 2/Задание 2/Задание 2/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace Задание_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(470, 36);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(268, 374);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(393, 54);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Определить количество предложений и слов";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(194, 369);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Сброс";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(40, 96);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(393, 170);
+            this.textBox.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(122, 290);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(223, 61);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "( ͡° ͜ʖ ͡°)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Лаба 2/Задание 2/Задание 2/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задание_2
+{
+    public partial class Form1 : Form
+    {
+        int p = 0;
+        int a = 0;
+        int eq = 0;
+        int  qwe=0;
+        int a1 = 0;
+        int asd=0;
+        int pz=0, a2 = 0;
+        string t, text="empty", te;
+        int o = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = richTextBox1.Text.ToString();
+            int countwords = 1, countpred = 0;
+            foreach (char q in text)
+            {
+                if (q == '.')
+                {
+                    countpred++;
+                    textBox.Text += "В предложении " + countpred + ": " + countwords + " слов." + Environment.NewLine;
+                    countwords = 0;
+                }
+                if (q == ' ') countwords++;
+            }
+            textBox.Text += "Всего в тексте "+ countpred +" предложений."+ Environment.NewLine;
+            KeyPreview = true;
+            string text1 = text;
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            p++;
+            if ((p==1) || ((pz-1)==a2))
+                {
+                if (o == 0)
+                {
+                    string text = richTextBox1.Text.ToString();
+                    te = text;
+                    o++;
+                }
+                foreach (char q in te)
+                {
+                    if (q == '.')
+                    {
+                        richTextBox1.SelectionStart = asd;
+                        richTextBox1.SelectionLength = a1;
+                        richTextBox1.SelectionBackColor = Color.Green;
+                        te = te.Substring(2);
+                        asd = asd + a1+2;
+                        a1 = 0;
+                        a2++;
+                        break;
+                    }
+                    else
+                    {
+                        a1++;
+                        te = te.Substring(1);
+                        a2++;
+                    }
+                }
+            }
+            else
+            {
+                if (qwe == 0)
+                {
+                    string text = richTextBox1.Text.ToString();
+                    qwe++;
+                    t = text;
+                }
+                foreach (char q in t)
+                {
+                    if ((q == ' ') || (q == '.'))
+                    {
+                        richTextBox1.SelectionStart = eq;
+                        richTextBox1.SelectionLength = a;
+                        richTextBox1.SelectionBackColor = Color.Red;
+                        t = t.Substring(1);
+                        eq =eq+ a+1;
+                        a = 0;
+                        pz++;
+                        break;
+                    }
+                    else a++;
+                    t = t.Substring(1);
+                    pz++;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+//{
+//    if (qwe == 0)
+//    {
+//        string text = richTextBox1.Text.ToString();
+//        qwe++;
+//        t = text;
+//    }
+//    else
+//    {
+//        t = text;
+//    }
+//    foreach (char q in t)
+//    {
+//        if (q == ' ')
+//        {
+//            richTextBox1.SelectionStart = eq;
+//            richTextBox1.SelectionLength = a;
+//            richTextBox1.SelectionBackColor = Color.Red;
+//            text = t.Substring(eq, a + 1);
+//            eq = a + 1;
+//            a = 0;
+//            break;
+//        }
+//        else a++;
+//    }
+//}
